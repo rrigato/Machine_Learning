388433271247fb68652df66c8dad77e64837f2c5
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2016-01-10 19:11:18 -0800

    Function validating solution is complete

--- Airbnb Model.R
+++ Airbnb Model.R
@@ -76,14 +76,21 @@ age_gender_bkts <- read.csv("C:\\Users\\Randy\\Downloads\\Kaggle Airbnb\\age_gen
 
 
 
-test = as.data.frame(matrix(nrow=10, ncol = 2))
-test = rename(test, c("V1" = "id", "V2" = "country")) 
-test[1:5,1] = '5uwns89zht'
-test[1:5,2] = 'NDF'
-test[6:10,1] = 'jtl0dijy2j'
-test[6:10,2] = 'NDF'
-test = test[,-c(3,4,5)]
-test
+data_frame = as.data.frame(matrix(nrow=10, ncol = 2))
+data_frame = rename(data_frame, c("V1" = "id", "V2" = "country")) 
+data_frame[1:5,1] = 'gxn3p5htnn'
+data_frame[1:5,2] = 'NDF'
+data_frame[6:10,1] = 'jh95kwisub'
+data_frame[6:10,2] = 'NDF'
+
+
+data_frame2 = as.data.frame(matrix(nrow=nrow(train), ncol = 2))
+data_frame2 = rename(data_frame2, c("V1" = "id", "V2" = "country")) 
+data_frame2[,1] = train$id
+sum(!is.na(data_frame2[,1]))
+data_frame2[,2] = 'NDF'
+
+NDCG(data_frame)
 ###################################################################################
 #	Normalized Discounted Cumulative Gain
 #
@@ -91,20 +98,69 @@ test
 #	fourth = .43, fifth = .386
 #
 #
-#
+#	#the data_frame that is provided must have 5 predicitions of id, country in
+#	order of importance
 #
 #
 #
 ###################################################################################
 NDCG <- function(data_frame){
+	
+	#Cumulative Gain total
+	total = 0
 
+	#counter for how many rows
+	i = 1
+	
+	#this is going to hold the country destination predictions from most to least likely
+	temp = numeric(5)
+
+	#outer while should iterate nrow(data_frame) /5 times
+	while (i < nrow(data_frame) )
+	{
+
+		#going to be the row in train where the correct destination is
+		row_num = 0
+
+		#gets the row_num from train that matches the id corresponding
+		#to the predictions in temp
+		row_num = (which(train[,1] == data_frame[i,1])[1] )
+
+
+		#input validation on what values row_num can be
+		if ((row_num < 1) || row_num > nrow(train) )
+		{
+			print("Error: unable to find row number of correct solution")
+			print("row_num:");
+			print(row_num);
+			return();
+		}
+
+
+		#setting temp as the five predicted countries.		
+		temp = data_frame[i:(i+4),2]
 		
+		score = 0
+		#determining which position the score is, should be one to five
+		score = (which(temp == train$country_destination[row_num]) [1])		
+		
+		#if score is zero you will end up dividing by zero so you only want to divide if 
+		#score is not zero
+		if (score >0)
+		{
+			total = total + 1/log2(1+score)
+		}
+		#increment i by 5 since there are 5 predictions
+		i = i + 5
+	
+	}
 
 
 
-
-
-
+	total = total / (nrow(data_frame) / 5)
+	print("The normalized Discounted Cumulative Gain is:");
+	print(total);
+	return(total);
 
 
 
