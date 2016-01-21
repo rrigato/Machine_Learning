e25c01139ad4c5e329e88bbecd30b2e296767a07
rrigato <rrigato@horizon.csueastbay.edu>
2016-01-20 16:49:27 -0800

    Added a simple plot from the Iris dataset

--- Shiny_Application.R
+++ Shiny_Application.R
@@ -2,17 +2,26 @@ install.packages("shiny")
 
 library(shiny)
 
+data(iris)
+
 ui <- 
 fluidPage(
 		#creates a slider object
 		sliderInput(inputId = "num", label = "Choose a number",   
 			   value = 12, min = 1, max = 25),
 		#need to put a comma between objects
-		plotOutput(outputId="hist")
+		plotOutput(outputId="main_plot")
 	   )
 
 server <- function (input, output)
 {
+	output$main_plot <- renderPlot({
+
+    				hist(		iris$Sepal.Length,
+     						 xlab = "Duration (minutes)",
+    				  			main = "Geyser eruption duration"
+					)
+				})
 }
 
 
